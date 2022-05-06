b1caf53ddff2ab5ee9e189feabfadcf9e6cd25bb
im-swapnilyelkar <im.swapnilyelkar@gmail.com>
2022-05-06 15:48:01 +0530

    2 files modified

--- src/modules/login/login.module.ts
+++ src/modules/login/login.module.ts
@@ -3,12 +3,11 @@ import { NgModule } from '@angular/core';
 import { MatInputModule } from '@angular/material/input';
 import { MatSliderModule } from '@angular/material/slider';
 import { LoginComponent } from './login.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [LoginComponent],
-  imports: [CommonModule, MatInputModule, MatSliderModule],
+  imports: [CommonModule, MatInputModule, MatSliderModule, ReactiveFormsModule],
   exports: [LoginComponent],
 })
-export class LoginModule {
-  loginForm:FormGroup
-}
+export class LoginModule {}
